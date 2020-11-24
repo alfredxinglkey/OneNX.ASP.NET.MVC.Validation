5953afeefe845bc3c1407ba568b358c422bb3576
xingyafei <xingyafei0801@outlook.com>
2020-11-24 09:18:51 +0800

    feat: add select demo(not finished).

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,12 +20,14 @@ namespace OneNX.ASP.NET.MVC.Validation
 							"~/Scripts/modernizr-*"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-						 "~/Scripts/bootstrap.js"));
+						 "~/Scripts/bootstrap.js",
+						 "~/Scripts/bootstrap-select.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 						 "~/Content/bootstrap.css",
 						 "~/Content/site.css",
-						 "~/Content/font-awesome.css"));
+						 "~/Content/font-awesome.css",
+						 "~/Content/bootstrap-select.css"));
 		}
 	}
 }
